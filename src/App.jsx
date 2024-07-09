8e7468d1c434a09ae21333ff190e5957c581b42a
SAMER FARES <123877088+FARESsamer23@users.noreply.github.com>
2024-07-09 03:43:32 -1200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
-import "./App.css"
-import About from "./components/About"
-import Blog from "./components/Blog"
-import Home from "./components/Home"
-import Myfooter from "./components/Myfooter"
-import Navbar from "./components/Navbar"
-import NewsLetter from "./components/NewsLetter"
-import Products from "./components/Products"
-import Services from "./components/Services"
-
+import "./App.css";
+import About from "./components/About";
+import Blog from "./components/Blog";
+import Home from "./components/Home";
+import Myfooter from "./components/Myfooter";
+import Navbar from "./components/Navbar";
+import NewsLetter from "./components/NewsLetter";
+import Products from "./components/Products";
+import Services from "./components/Services";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
-    <div className="App" basename={'LandingPageDesigne'}>
-    <Navbar/>
-   <Home/>
-   <Services/>
+    <div className="App">
+      <Router basename={'/LandingPageDesigne'}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+      <Services/>
    <About/>
    <Products/>
    <Blog/>
    <NewsLetter/>
    <Myfooter/>
+      
     </div>
-  )
+  );
 }
